828b347e93d5b4a9a1b7b1a3e693e7ce627bd6ce
zhq092012 <36983262+zhq092012@users.noreply.github.com>
2020-02-22 20:31:30 +0800

    class类组件

--- src/index.js
+++ src/index.js
@@ -1,30 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-// const app=<h1>webcome to react</h1>
-// const createApp = props => {
-//   return (
-//     <div>
-//       {/* 只要在jsx里插入js代码，就加一层{} */}
-//       <h1>webcome to {props.title}</h1>
-//       <p>优秀的{props.title}</p>
-//     </div>
-//   );
-// };
-// const app = createApp({
-//   title: "react 16.8"
-// });
-
-//创建组件的第一种方式就是使用=>函数，名字要大写
-const App = props => {
-  return (
-    <div>
-      {/* 只要在jsx里插入js代码，就加一层{} */}
-      <h1>webcome to {props.title}</h1>
-      <p>优秀的{props.title}</p>
-    </div>
-  );
-};
-ReactDOM.render(
-  <App title="react 1901"></App>,
+import React, { Component } from "react";
+import { render } from "react-dom";
+const Header = () => <h1>类组件</h1>;
+//定义组件的第二种方式
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header></Header>
+        <p>{this.props.desc}</p>
+      </div>
+    );
+  }
+}
+//类组件渲染的原理
+// const app = new App({
+//   desc: "类组件继承React.Component"
+// }).render();
+render(
+  <App desc="类组件继承React.Component" />,
   document.querySelector("#root")
 );
