bc08e30f389f35d336e0983ffe39b6ea5f1ad8a9
zhq092012 <36983262+zhq092012@users.noreply.github.com>
2020-02-23 20:07:35 +0800

    react生命周期函数 方式重复渲染

--- src/components/TodoList/TodoItem.js
+++ src/components/TodoList/TodoItem.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 const noop = () => {};
 export default class TodoItem extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     completedText: ""
+  //   };
+  // }
+  // static getDrivedStateFromProps(props) {
+  //   return {
+  //     completedText: props.isCompleted ? "完成" : "未完成"
+  //   };
+  // }
   handleCheckboxChange = () => {
     // this.props.onCompletedChecked &&
     // this.props.onCompletedChecked(this.props.id);
     const { onCompletedChecked = noop, id } = this.props; //解构this.props
     onCompletedChecked && onCompletedChecked(id);
   };
+  //解决重复render
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.isCompleted !== this.props.isCompleted;
+  }
   render() {
+    console.log(`TodoItem${this.props.title}Render`);
+
     const { isCompleted, title } = this.props; //解构this.props
     return (
       <li>
